579c398b046cef2b463adf9d3e5553947240bd78
Thore Buecking <rmaptbu@ucl.ac.uk>
2014-10-30 15:45:41 +0000

    restructured greengraph code -> imports at the beginning

--- greengraph.py
+++ greengraph.py
@@ -1,28 +1,24 @@
-### "geolocation"
 from geolocate import geolocate
+from mapurl import map_at
+import imagemanipulation as im
+from locationsequence import location_sequence
+
+#find london location in coordinates
 london_location=geolocate("London")
 print london_location
 
 ### "URL"
-from mapurl import map_at
+
 map_response=map_at(51.5072, -0.1275, zoom=10)
 url=map_response.url
 print url
 
 ### "png"
-import imagemanipulation as im
+
 print im.count_green_in_png(map_at(*london_location))
 
 ### "points"
 
-from numpy import linspace
-def location_sequence(start,end,steps):
-  # Would actually prefer this if steps
-  # were deduced from zoomlevel
-  # But need projection code for that
-  lats=linspace(start[0],end[0],steps)
-  longs=linspace(start[1],end[1],steps)
-  return zip(lats,longs)
 
 [im.count_green_in_png(map_at(*location,zoom=10,satellite=True))
             for location in location_sequence(
