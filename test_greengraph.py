a42ef47905c15773fb9172bfd89a685c23a9c1ed
Thore Buecking <rmaptbu@ucl.ac.uk>
2014-10-30 15:36:45 +0000

    moved geolocate,URL, and png into individual modules

--- test_greengraph.py
+++ test_greengraph.py
@@ -2,5 +2,8 @@ from PIL import Image
 from nose.tools import assert_equal
 import math
 def test_greeter():
+    #regression test
     import greengraph
-    assert_equal(Image.open("greengraph_test.png").histogram(), Image.open("greengraph.png").histogram())
+    assert_equal(Image.open("greengraph_test.png").histogram(), 
+	Image.open("greengraph.png").histogram())
+	#unit tests
