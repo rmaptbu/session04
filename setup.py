d605ecb3a64cd954459821e725f704b3038d746a
Thore Buecking <rmaptbu@ucl.ac.uk>
2014-10-31 14:12:08 +0000

    added setup file, changed folder structure

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "Greengraph",
+    version = "0.1",
+    packages = find_packages(exclude=['*test']),
+    install_requires = ['geopy', 'png', 'itertools', 'StringIO', 'numpy', 'requests']
+)
