2ae17fc000ba6c9eb12634a6f2ebee8537af0d50
Thore Buecking <rmaptbu@ucl.ac.uk>
2014-11-14 15:21:32 +0000

    added init script, turned greengraph into function greengraph.process, addes __init__.py

--- greengraph/greengraph.py
+++ greengraph/greengraph.py
@@ -3,28 +3,28 @@ from mapurl import map_at
 import imagemanipulation as im
 from locationsequence import location_sequence
 
+def process():
+	#count number of green pixels between london and birmingham
+	[im.count_green_in_png(map_at(*location,zoom=10,satellite=True))
+				for location in location_sequence(
+					geolocate("London"),
+					geolocate("Birmingham"),
+					10)]
 
-#count number of green pixels between london and birmingham
-[im.count_green_in_png(map_at(*location,zoom=10,satellite=True))
-            for location in location_sequence(
-                geolocate("London"),
-                geolocate("Birmingham"),
-                10)]
 
+	### "save"
+	import matplotlib
+	matplotlib.use('Agg')
+	import matplotlib.pyplot as plt
 
-### "save"
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
+	with open('green.png','w') as green:
+		green.write(im.show_green_in_png(map_at(51.5073509, -0.1277583,
+			zoom=10,satellite=True)))
 
-with open('green.png','w') as green:
-    green.write(im.show_green_in_png(map_at(51.5073509, -0.1277583,
-        zoom=10,satellite=True)))
-
-plt.plot([
-    im.count_green_in_png(
-        map_at(*location,zoom=10,satellite=True))
-          for location in location_sequence(
-              geolocate("London"),
-              geolocate("Birmingham"),10)])
-plt.savefig('greengraph.png')
+	plt.plot([
+		im.count_green_in_png(
+			map_at(*location,zoom=10,satellite=True))
+			  for location in location_sequence(
+				  geolocate("London"),
+				  geolocate("Birmingham"),10)])
+	plt.savefig('greengraph.png')
--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "Greengraph",
+    name = "greengraph",
     version = "0.1",
-    packages = find_packages(exclude=['*test']),
+	packages = find_packages(exclude=['*test']),
+	scripts = ['scripts/greengraph'],    
     install_requires = ['geopy', 'pypng', 'numpy', 'requests']
 )
